d6323277ef59c7dc5ce45c2b530798bb84404a01
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-03-22 15:49:35 +0100

    feat: Middleware and error handlers

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const MONGO_URI = process.env.MONGO_URI;
+// const MONGO_URI = process.env.MONGO_URI;
+console.log("Why is this happening?", process.env.MONGO_URI);
 
 const connectDB = () => {
   try {
-    const conn = mongoose.connect(MONGO_URI);
+    const conn = mongoose.connect(process.env.MONGO_URI);
     console.log("Database connection");
   } catch (err) {
     console.log("Database Error", err);
