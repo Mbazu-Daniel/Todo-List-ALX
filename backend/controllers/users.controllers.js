03d4d9e65d99f3363acd164b756c557afaa534d6
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-03-30 04:36:32 +0100

    feat: created user login controllers and routes

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/users.models");
+const { generateToken } = require("../config/jwtToken");
 
 const createUser = asyncHandler(async (req, res) => {
   const { email, username } = req.body;
@@ -19,6 +20,28 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password, username } = req.body;
+  // check if user already exists
+  const user = await User.findOne({
+    $or: [{ email: email }, { username: username }],
+  });
+
+  if (user && (await user.comparePassword(password))) {
+    res.status(201).json({
+      _id: user?.id,
+      username: user?.username,
+      email: user?.email,
+
+      token: generateToken(user?._id),
+    });
+  } else {
+    throw new Error("Invalid Credentials");
+  }
+});
+
 module.exports = {
   createUser,
+  loginUser,
 };
