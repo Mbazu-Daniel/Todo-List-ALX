ee35b3a62fb1e075ec5576528d79981894fa7236
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-03-30 04:27:08 +0100

    security: encrypt password when users register a new account and also return a token when users login

--- backend/models/users.models.js
+++ backend/models/users.models.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -20,5 +21,15 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model("User", UserSchema);
